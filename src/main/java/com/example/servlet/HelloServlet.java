375e61f6bc2f9c0bced810a461882aaa1b777b03
gorlova-natalya <natashka26102013@gmail.com>
2022-10-02 14:38:14 +0300

    commit lesson 22

---
+++ src/main/java/com/example/servlet/HelloServlet.java
@@ -0,0 +1,34 @@
+package com.example.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class HelloServlet extends HttpServlet {
+
+    private AtomicInteger count = new AtomicInteger(0);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write("Count " + Integer.toString(count.get()));
+            writer.write(" ContextPath " + req.getContextPath());
+            writer.write(" URI " + req.getRequestURI());
+            writer.write(" ServletPath " + req.getServletPath());
+            writer.write(" LocalAddr " + req.getLocalAddr());
+            writer.write(" Method " + req.getMethod());
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try (PrintWriter writer = resp.getWriter()) {
+            writer.write(Integer.toString(count.incrementAndGet()));
+        }
+    }
+}
